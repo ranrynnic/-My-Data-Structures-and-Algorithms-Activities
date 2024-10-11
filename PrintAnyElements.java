b1653f14c2a32ae63377314c66bb6b6710b65d00
ranrynnic <ranolaryannicoli@gmail.com>
2024-10-11 20:24:09 +0800

    Add files via upload

---
+++ PrintAnyElements.java
@@ -0,0 +1,9 @@
+ public class PrintAnyElements{
+     public static void main(String[] args) {
+         // Initialize the array
+         int[] num = {10, 20, 30, 40, 50};
+
+         // Print the second element in the array
+         System.out.println("The second element is: " + num[1]);
+     }
+}
