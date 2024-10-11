28f99ee166e8606ba02032fe67f9ed38b3d76f9f
ranrynnic <ranolaryannicoli@gmail.com>
2024-10-11 20:22:39 +0800

    Add files via upload

---
+++ ArrayInitialization.java
@@ -0,0 +1,11 @@
+public class ArrayInitialization {
+    public static void main(String[] args) {
+        // Initializing an array with specific values
+        int[] num = {1, 2, 3, 4, 5};
+
+        // Printing of the arrays
+        for (int a = 0; a < num.length; a++) {
+            System.out.println(num[a]);
+        }
+    }
+}
